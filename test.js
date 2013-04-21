819e37f33e2b8426a24a3b77521e6103dcd989be
gaoxiaochen <gxcsoccer@126.com>
2013-04-22 05:14:38 +0800

    add more functions

--- test.js
+++ test.js
@@ -2,7 +2,11 @@ var Enumerable = require("./lib/Enumerable");
 
 var a = Enumerable.fromArray([1, 2, 3, 4, 5, 6]).where(function(item, index) {
 	return item > 2;
-}).forEach(function(item) {
+}).select(function(item) {
+	return item + 100;
+}).orderByDescending(function(a) {
+	return a;
+}, true).take(2).forEach(function(item) {
 	console.log(item);
 }).toArray();
 
